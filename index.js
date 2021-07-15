5012b7cc9844fdcf6e570bcdde7fcab820dcae67
Charles <chall70101@gmail.com>
2021-07-15 15:41:14 -0600

    commit tree 0800fba1b50ca8308f040a0f5433da7ee3f5b57f

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+let multiply
+
+multiply = num1 * num2;
+
+let random
+
+random = Math.round(Math.random() *100)
+
+let num3 = 44
+let num4 = 10
+let mod 
+
+mod = (num3 % num4)
+
+let max
+
+max = Math.max(1,4,6,20)
